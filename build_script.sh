52e0b4612c6e997cbf04ecd84ee7e3b11ab002e8
dstorer-cap <daniel.storer@capgemini.com>
2024-01-23 16:31:48 +0000

    Modified path in build script

--- build_script.sh
+++ build_script.sh
@@ -4,6 +4,6 @@ sudo rm -rf picolib-microkit/
 sudo mkdir picolib-microkit
 cd picolib-microkit
 mkdir ../picolibc_build
-sudo ../scripts/do-aarch64-configure-nocrt -Dprefix=../picolibc_build
+sudo ../scripts/do-aarch64-configure-nocrt -Dprefix=${PWD}/../picolibc_build
 sudo ninja 
 sudo ninja install
